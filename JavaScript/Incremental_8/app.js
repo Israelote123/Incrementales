2568954e904327720b10eb579daa8e5e0310d891
Antonio Santos <jantonios44@hotmail.com>
2021-11-25 11:48:37 -0600

    Incremental 8 devuleve mes con letra

---
+++ JavaScript/Incremental_8/app.js
@@ -0,0 +1,59 @@
+mes(2)
+
+function mes(valor)
+{
+   
+    switch(valor)
+    {
+        case 1:
+        return console.log("Enero")
+        break
+
+        case 2:
+        return console.log("Febrero")
+        break
+        
+        case 3:
+        return console.log("Marzo")
+        break
+
+        case 4:
+        return console.log("Abril")
+        break
+
+        case 5:
+        return console.log("Mayo")
+        break
+
+        case 6:
+        return console.log("Junio")
+        break
+
+        case 7:
+        return console.log("Julio")
+        break
+
+        case 8:
+        return console.log("Agosto")
+        break
+
+        case 9:
+        return console.log("Septiembre")
+        break
+
+        case 10:
+        return console.log("Octubre")
+        break
+
+        case 11:
+        return console.log("Noviembre")
+        break
+
+        case 12:
+        return console.log("Diciembre")
+        break
+
+        default:
+            return console.log("No escogio ninguan opcion")    
+    }
+}
