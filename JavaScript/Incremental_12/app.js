1049ed59f7f6ae0079eb264a35debce3aac42518
Antonio Santos <jantonios44@hotmail.com>
2021-12-01 15:12:39 -0600

    seudocodigo y programa

---
+++ JavaScript/Incremental_12/app.js
@@ -0,0 +1,51 @@
+var A, B, C, D;
+var s = 0;
+
+while (s != "salir") {
+    A = parseInt(prompt("Ingrese Primer numero"));
+    B = parseInt(prompt("Ingrese Segundo numero"));
+    while (A == B) {
+        if (A == B) {
+            alert("El segundo numero se repite, ingrese de nuevo segundo numero");
+            B = parseInt(prompt("Ingrese Segundo numero"));
+        }
+
+    }
+    C = parseInt(prompt("Ingrese Tercer numero"));
+
+    while (A == C || B == C) {
+        if (A == C || B == C) {
+            alert("El tercer numero se repite, ingrese de nuevo tercer numero");
+            C = parseInt(prompt("Ingrese tercer numero"));
+        }
+    }
+
+
+    if (A < B) {
+        D = A;
+        A = B;
+        B = D;
+    }
+
+
+    D = 0;
+    if (B < C) {
+        D = B;
+        B = C;
+        C = D;
+    }
+    D = 0
+    if (A < B) {
+        D = A;
+        A = B;
+        B = D;
+    }
+
+    console.log("El mayor numero de likes en su foto es:" + A);
+    console.log("El menor numero de likes en su foto es: " + C);
+    alert("El mayor numero de likes en su foto es: " + A+"\ny el menor numero de likes en su foto es: " + C)
+    s = prompt("Si desea ingresar likes de otra foto ingrese 1 de lo contario salir");
+}
+
+
+alert("Gracias por usar nuestra aplicacion")
