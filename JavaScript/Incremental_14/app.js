9a0fae48e95a857a415d1428e86cd78c0270326d
Israelote123 <joseprinsisrael@gmail.com>
2021-12-03 17:44:51 -0600

    Correccion incremental_14

--- JavaScript/Incremental_14/app.js
+++ JavaScript/Incremental_14/app.js
@@ -10,7 +10,7 @@ function getUsuariosGit(){
         })
         .then(() => {
             document.getElementById("photo").innerHTML = '<img class="logo" src="'+ resultado.results[0].picture.large +'" alt="foto">' + '<br>' + resultado.results[0].name.first + ' ' + resultado.results[0].name.last;
-            document.getElementById("profile").innerHTML = resultado.results[0].location.country + '<br>Telefono celular: ' + resultado.results[0].phone + '<br>Genero: ' + resultado.results[0].gender + '<br>Edad: ' + resultado.results[0].dob.age +'<br> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis placeat, rerum natus dolorum inventore ab quos facere quidem illo dolorem tempore eius porro deleniti, tempora, vel voluptates et velit veniam.';
+            document.getElementById("profile").innerHTML = 'Pais: ' + resultado.results[0].location.country + '<br>Telefono celular: ' + resultado.results[0].phone + '<br>Genero: ' + resultado.results[0].gender + '<br>Edad: ' + resultado.results[0].dob.age +'<br> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis placeat, rerum natus dolorum inventore ab quos facere quidem illo dolorem tempore eius porro deleniti, tempora, vel voluptates et velit veniam.';
             document.getElementById("data").innerHTML = 'Fecha de registro: ' + resultado.results[0].registered.date;
         })
         .catch(error => {
