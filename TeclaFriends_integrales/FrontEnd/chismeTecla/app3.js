7d3b16cb3ddf4015290d1f5a9d3b0b7cc803d519
Antonio Santos <jantonios44@hotmail.com>
2021-12-27 18:29:53 -0600

    pagina chismetecla

---
+++ TeclaFriends_integrales/FrontEnd/chismeTecla/app3.js
@@ -0,0 +1,36 @@
+const url2 = 'https://meme-api.herokuapp.com/gimme'
+let resultado;
+let imagen;
+var para=[];
+var comparacion;
+
+function getMemes(i) {
+    fetch(url2)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            resultado = json;
+        })
+
+        .then(() => {
+            if(comparacion!=resultado.url)
+            {
+                comparacion=resultado.url
+                let att = document.createAttribute("class");       // Create a "class" attribute
+                att.value = "democlass";                           // Set the value of the class attribute
+                para[i] = document.createElement('div');
+                para[i].setAttributeNode(att); 
+                para[i].innerHTML = '<br>'+'<img class="meme" src="' + resultado.url + '" alt="foto">' + '<br>';
+                document.body.appendChild(para[i]); 
+            }
+         })
+        .catch(error => {
+            console.error("Error al solicitar los datos")
+            document.getElementById("profile").innerHTML = 'Conexion fallida ;-; unu :c';
+        })
+}
+
+for (let i = 0; i < 200; i++) {
+      getMemes(i);
+}
+
