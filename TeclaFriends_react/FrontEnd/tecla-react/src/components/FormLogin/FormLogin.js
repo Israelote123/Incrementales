d5be5be6cfc326280e34bf5ce5bd3b66dab9dffb
Israelote123 <joseprinsisrael@gmail.com>
2022-01-20 16:40:47 -0600

    NavLink en login

--- TeclaFriends_react/FrontEnd/tecla-react/src/components/FormLogin/FormLogin.js
+++ TeclaFriends_react/FrontEnd/tecla-react/src/components/FormLogin/FormLogin.js
@@ -1,29 +1,30 @@
 import './FormLogin.css'
+import {NavLink} from 'react-router-dom'
 
 function FormLogin(){
     return  (<main>
-            <div class="container login">
-                <div class="row">
-                    <div class="col-lg">
-                        <form class="col" onsubmit="event.preventDefault();">
-                            <div class="form-floating">
-                                <input type="email" class="form-control text"
+            <div className="container login">
+                <div className="row">
+                    <div className="col-lg">
+                        <form className="col" onsubmit="event.preventDefault();">
+                            <div className="form-floating">
+                                <input type="email" className="form-control text"
                                     name="user_mail"
                                     placeholder="user@mail.com" />
                                 <label for="floatingInput mail">Correo
                                     electrónico:</label><br />
                             </div>
-                            <div class="form-floating mb-3">
-                                <input type="password" class="form-control text"
+                            <div className="form-floating mb-3">
+                                <input type="password" className="form-control text"
                                     id="password" name="user_password"
                                     placeholder="Password" />
                                 <label for="floatingPassword password">Contraseña:</label>
                             </div>
-                            <button class="btn btn-primary mb-3"
+                            <button className="btn btn-primary mb-3"
                                 id="style-button" onclick="inicioSesion()">Iniciar
                                 sesión</button>
-                            <a class="btn btn-primary" id="style-button3" href="/new-user">¿No tienes
-                                cuenta? Registrate</a>
+                            <NavLink className="btn btn-primary" id="style-button3" to="/new-user">¿No tienes
+                                cuenta? Registrate</NavLink>
                         </form>
                     </div>
                 </div>
